4a32b41ad946ac9ee324caab9d484b0b89c0d36e
KudosAbhay <kudosabhay@gmail.com>
2017-08-28 11:19:55 +0000

    Updated

--- Server.py
+++ Server.py
@@ -10,29 +10,34 @@ print("Socket successfully created")
 # reserve a port on your computer in our
 # case it is 12345 but it can be anything
 port = 8000
-
+c = None
 
 # Next bind to the port
 # we have not typed any ip in the ip field
 # instead we have inputted an empty string
 # this makes the server listen to requests
 # coming from other computers on the network
-s.bind(('', port))
-print("socket binded to {}" .format(port))
+try:
+	s.bind(('', port))
+	print("socket binded to {}" .format(port))
 
-# put the socket into listening mode
-s.listen(5)
-print("socket is listening")
+	# put the socket into listening mode
+	s.listen(5)
+	print("socket is listening")
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-   # Establish connection with client.
-   c, addr = s.accept()
-   print("Got connection from:\t{}".format(addr))
+	# a forever loop until we interrupt it or
+	# an error occurs
+	while True:
+	   # Establish connection with client.
+	   c, addr = s.accept()
+	   print("Got connection from:\t{}".format(addr))
 
-   # send a thank you message to the client.
-   c.send(b'Thank you for connecting')
+	   # send a thank you message to the client.
+	   c.send(b'Thank you for connecting')
 
-   # Close the connection with the client
-   c.close()
+	   # Close the connection with the client
+	   #c.close()
+
+except KeyboardInterrupt:
+	print("KeyboardInterrupt Caught.\nClosing Server Socket on port:\t{}".format(port))
+	c.close()
