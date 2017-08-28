88306025da8b16d5775def9d2cb25348456f4e3f
KudosAbhay <kudosabhay@gmail.com>
2017-08-28 11:54:10 +0000

    Catching Exceptions for Connections now

--- Server.py
+++ Server.py
@@ -3,41 +3,44 @@
 # first of all import the socket library
 import socket
 
-# next create a socket object
-s = socket.socket()
-print("Socket successfully created")
-
 # reserve a port on your computer in our
 # case it is 12345 but it can be anything
 port = 8000
-c = None
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-try:
+def startListening():
+	#Create a Socket Object
+	s = socket.socket()
+	print("\nCreating Socket..")
+	# Next bind to the port
+	# we have not typed any ip in the ip field
+	# instead we have inputted an empty string
+	# this makes the server listen to requests
+	# coming from other computers on the network
 	s.bind(('', port))
-	print("socket binded to {}" .format(port))
+	print("\nSocket binded to {}" .format(port))
 
-	# put the socket into listening mode
+	# Put the socket into listening mode
 	s.listen(5)
-	print("socket is listening")
+	print("Socket is listening")
 
-	# a forever loop until we interrupt it or
-	# an error occurs
-	while True:
-	   # Establish connection with client.
-	   c, addr = s.accept()
-	   print("Got connection from:\t{}".format(addr))
+	# Forever loop until we interrupt it or until an error occurs
+	try:
+		while True:
+		   	# Establish connection with client.
+		   	c, addr = s.accept()
+		   	print("Got connection from:\t{}".format(addr))
 
-	   # send a thank you message to the client.
-	   c.send(b'Thank you for connecting')
+		   	# send a thank you message to the client.
+		   	c.send(b'Thank you for connecting')
 
-	   # Close the connection with the client
-	   #c.close()
+		   	# Close the connection with the client
+		   	#c.close()
+	except KeyboardInterrupt:
+		print("\nStopping all connections.\nClosing Server Socket on port:\t{}".format(port))
+		c.close()
 
-except KeyboardInterrupt:
-	print("KeyboardInterrupt Caught.\nClosing Server Socket on port:\t{}".format(port))
-	c.close()
+
+try:
+	startListening()
+except UnboundLocalError:
+	print("Server stopped unexpectedly before connecting to Clients")
